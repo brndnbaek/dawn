f4f24ec1a77b76e86f41a5852bc05a88f96182fe
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2022-02-04 16:42:57 -0500

    initial setup (duplicated from original work)

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+
+module.exports = {
+    plugins: [
+      require('postcss-import'),
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // purgecss({
+      //   content: [
+      //     './layout/*.liquid',
+      //     './templates/*.liquid',
+      //     './sections/*.liquid',
+      //     './snippets/*.liquid',
+      //   ],
+      //   defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || [],
+      // })
+    ]
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: [
+      './layout/*.liquid',
+      './templates/*.liquid',
+      './sections/*.liquid',
+      './snippets/*.liquid',
+    ],
+    // content: [
+    //   './snippets/collection-within-collection.liquid',
+    //   './templates/pages.character.liquid',
+    // ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'lt-yellow': '#EFBC61',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+}
