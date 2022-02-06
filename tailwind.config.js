4adc434937b355bc115a60ec5c2ff5bd412f2442
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2022-02-05 20:03:41 -0500

    working env revised, series list page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-    // content: [
-    //   './layout/*.liquid',
-    //   './templates/*.liquid',
-    //   './sections/*.liquid',
-    //   './snippets/*.liquid',
-    // ],
-    // content: [
-    //   './snippets/collection-within-collection.liquid',
-    //   './templates/pages.character.liquid',
-    // ],
-  
-  // darkMode: false, // or 'media' or 'class'
+    content: [
+      './layout/*.liquid',
+      './templates/*.liquid',
+      './sections/*.liquid',
+      './snippets/*.liquid',
+    ],
+    
   theme: {
     extend: {
       colors: {
         'lt-yellow': '#EFBC61',
+      },
+      spacing: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '3/4': '75%',
+        
       }
     },
   },
