5b71fd7c4e7ba2adc35ee747ccea6042f4a97107
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2022-04-11 18:02:08 -0400

    monday work finish - almost ready to publish

--- assets/LT_gsap.js
+++ assets/LT_gsap.js
@@ -1,6 +1,7 @@
 gsap.fromTo(".color_bar", {scaleX:0.9, opacity:1}, {scaleX:1, opacity:.75, duration:1.5, stagger:0.3, yoyo:true, repeat:-1})
 gsap.fromTo(".color_bar2", {scaleX:.9, opacity:1}, {scaleX:1, opacity:0.75, duration:1.5, stagger:0.3, yoyo:true, repeat:-1})
 
+//kpop solo artists banner animation
 console.clear();
 let count = 0;
 const targets = document.querySelectorAll(".LT_slides");
@@ -10,11 +11,11 @@ gsap.set(targets[0], { xPercent: 0 });
 function slideIt() {
   gsap.to(targets[count], { xPercent: 100 });
   count = count < targets.length - 1 ? ++count : 0;
-  gsap.fromTo(targets[count], { xPercent: -100 }, { xPercent: 0, duration:3 });
-  gsap.to({}, { duration: 15, onComplete: slideIt });
+  gsap.fromTo(targets[count], { xPercent: -100, scale: 2, ease: 'Power2.out' }, { xPercent: 0, scale: 1, ease: 'Power2.out', duration:3 });
+  gsap.to({}, { duration: 12, onComplete: slideIt });
 }
 
-gsap.delayedCall(15, () => slideIt());
+gsap.delayedCall(12, () => slideIt());
 
 // let btstl = gsap.timeline({
 //     scrollTrigger: {
--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
-// const purgecss = require('@fullhuman/postcss-purgecss')
+//const purgecss = require('@fullhuman/postcss-purgecss')
 
 
 module.exports = {
     plugins: [
       require('postcss-import'),
       require('tailwindcss'),
-      require('autoprefixer'),
+      require('autoprefixer')
       // purgecss({
       //   content: [
       //     './layout/*.liquid',
