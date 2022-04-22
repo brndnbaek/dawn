224e4e5e927abb14b8cfa0d58608b9b34ceceed6
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2022-04-22 17:20:26 -0400

    add to cart popup problem!

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,18 @@
 //const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const postcssImport = require('postcss-import')
 
+// module.exports = {
+//     plugins: [
+//       require('postcss-import'),
+//       require('tailwindcss'),
+//       require('autoprefixer'),
+//       process.env.NODE_ENV === 'production' ? cssnano({preset: 'default'}) :null,
+//     ]
+//   }
 
-module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('autoprefixer')
-      // purgecss({
-      //   content: [
-      //     './layout/*.liquid',
-      //     './templates/*.liquid',
-      //     './sections/*.liquid',
-      //     './snippets/*.liquid',
-      //   ],
-      //   defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || [],
-      // })
-    ]
-  }
+const plugins = process.env.NODE_ENV === 'production' ? [tailwindcss,autoprefixer,postcssImport,cssnano] : [tailwindcss,autoprefixer,postcssImport]
+
+module.exports = { plugins }
