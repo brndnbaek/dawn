19db19e13448a26a5fe1ba11dc1e5f1a85e33aea
Brandon Baek <baekpro@gmail.com>
2023-02-04 04:08:00 -0500

    first setup on 2019imac

---
+++ assets/brandon_script.js
@@ -0,0 +1,44 @@
+Shopify.Cart = Shopify.Cart || {};
+    
+      Shopify.Cart.InsuranceAddon = {};
+    
+      Shopify.Cart.InsuranceAddon.set = function() {
+        var headers = new Headers({ 'Content-Type': 'application/json' });
+    
+        var request = {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({ updates: { {{ id }}: 1 }, attributes: { 'insurance-addon': true } })
+        };
+        fetch('/cart/update.js', request)
+        .then(function() {
+          location.href = '/cart';
+        });
+      }
+    
+      Shopify.Cart.InsuranceAddon.remove = function() {
+        var headers = new Headers({ 'Content-Type': 'application/json' });
+    
+        var request = {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({ updates: { {{ id }}: 0 }, attributes: { 'insurance-addon': '' } })
+        };
+        fetch('/cart/update.js', request)
+        .then(function() {
+          location.href = '/cart';
+        });
+      }
+
+				Shopify.Cart.InsuranceAddon.reset = function() {
+					var headers = new Headers({ 'Content-Type': 'application/json' });
+					var request = {
+						method: 'POST',
+						headers: headers,
+						body: JSON.stringify({ updates: { {{ id }}: 0 }, attributes: { 'insurance-addon': true } })
+					};
+					fetch('/cart/update.js', request)
+					.then(function() {
+						location.href = '/cart';
+					});
+				}
