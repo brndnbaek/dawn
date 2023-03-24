19a9d96d89933b588d3431793746a87e9b2c057d
Brandon Baek <baekpro@gmail.com>
2023-03-24 12:01:18 -0400

    title no format applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,47 +2,47 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
-    content: [
-      './layout/*.liquid',
-      './templates/*.liquid',
-      './sections/*.liquid',
-      './snippets/*.liquid',
-    ],
-    
+  content: [
+    "./layout/*.liquid",
+    "./templates/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+  ],
+
   theme: {
     extend: {
       gridTemplateRows: {
-        '7': 'repeat(7, minmax(0, 1fr))',
-        '8': 'repeat(8, minmax(0, 1fr))',
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
       },
       gridTemplateColumns: {
-        '24': 'repeat(24, minmax(0, 1fr))',
+        24: "repeat(24, minmax(0, 1fr))",
       },
       aspectRatio: {
-        'vertical': '9 / 16',
+        vertical: "9 / 16",
       },
       colors: {
-        'lt-yellow': '#EFBC61',
-        'lt-white': '#f7f7f5',
-        'lt-apricot': '#f09174',
+        "lt-yellow": "#EFBC61",
+        "lt-white": "#f7f7f5",
+        "lt-apricot": "#f09174",
       },
       spacing: {
-        '1/2': '50%',
-        '1/3': '33.333333%',
-        '2/3': '66.666667%',
-        '1/4': '25%',
-        '3/4': '75%',
-        
-      }
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+        "1/4": "25%",
+        "3/4": "75%",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
   ],
   future: {
     purgeLayersByDefault: true,
   },
-}
+};
